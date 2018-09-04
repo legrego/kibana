d3babeabac4cf30832b42883d9b26f6f8ccd5f4d
kobelb <brandon.kobel@elastic.co>
2018-09-04 17:43:38 -0400

    Moving over the spaces only saved objects tests

---
+++ x-pack/test/saved_object_api_integration/spaces_only/apis/saved_objects/delete.js
@@ -0,0 +1,102 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import expect from 'expect.js';
+import { SPACES } from '../../../common/lib/spaces';
+import { getUrlPrefix, getIdPrefix } from '../../../common/lib/space_test_utils';
+import { DEFAULT_SPACE_ID } from '../../../../../plugins/spaces/common/constants';
+
+export default function ({ getService }) {
+  const supertest = getService('supertest');
+  const esArchiver = getService('esArchiver');
+
+  describe('delete', () => {
+
+    const expectEmpty = () => (resp) => {
+      expect(resp.body).to.eql({});
+    };
+
+    const expectNotFound = (type, id) => (resp) => {
+      expect(resp.body).to.eql({
+        statusCode: 404,
+        error: 'Not Found',
+        message: `Saved object [${type}/${id}] not found`
+      });
+    };
+
+    const deleteTest = (description, { spaceId, tests }) => {
+      describe(description, () => {
+        before(() => esArchiver.load('saved_objects/spaces'));
+        after(() => esArchiver.unload('saved_objects/spaces'));
+
+        it(`should return ${tests.spaceAware.statusCode} when deleting a space-aware doc`, async () => (
+          await supertest
+            .delete(`${getUrlPrefix(spaceId)}/api/saved_objects/dashboard/${getIdPrefix(spaceId)}be3733a0-9efe-11e7-acb3-3dab96693fab`)
+            .expect(tests.spaceAware.statusCode)
+            .then(tests.spaceAware.response())
+        ));
+
+        it(`should return ${tests.notSpaceAware.statusCode} when deleting a non-space-aware doc`, async () => (
+          await supertest
+            .delete(`${getUrlPrefix(spaceId)}/api/saved_objects/space/space_2`)
+            .expect(tests.notSpaceAware.statusCode)
+            .then(tests.notSpaceAware.response())
+        ));
+
+        it(`should return ${tests.inOtherSpace.statusCode} when deleting a doc belonging to another space`, async () => {
+          const documentId = `${getIdPrefix('space_2')}be3733a0-9efe-11e7-acb3-3dab96693fab`;
+
+          let expectedObjectId = documentId;
+
+          if (spaceId !== DEFAULT_SPACE_ID) {
+            expectedObjectId = `${spaceId}:${expectedObjectId}`;
+          }
+
+          await supertest
+            .delete(`${getUrlPrefix(spaceId)}/api/saved_objects/dashboard/${documentId}`)
+            .expect(tests.inOtherSpace.statusCode)
+            .then(tests.inOtherSpace.response('dashboard', expectedObjectId));
+        });
+      });
+    };
+
+    deleteTest(`in the default space`, {
+      ...SPACES.DEFAULT,
+      tests: {
+        spaceAware: {
+          statusCode: 200,
+          response: expectEmpty
+        },
+        notSpaceAware: {
+          statusCode: 200,
+          response: expectEmpty
+        },
+        inOtherSpace: {
+          statusCode: 404,
+          response: expectNotFound
+        }
+      }
+    });
+
+    deleteTest(`in the current space (space_1)`, {
+      ...SPACES.SPACE_1,
+      tests: {
+        spaceAware: {
+          statusCode: 200,
+          response: expectEmpty
+        },
+        notSpaceAware: {
+          statusCode: 200,
+          response: expectEmpty
+        },
+        inOtherSpace: {
+          statusCode: 404,
+          response: expectNotFound
+        }
+      }
+    });
+  });
+}
---
+++ x-pack/test/saved_object_api_integration/spaces_only/apis/saved_objects/get.js
@@ -0,0 +1,120 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import expect from 'expect.js';
+import { getIdPrefix, getUrlPrefix } from '../../../common/lib/space_test_utils';
+import { SPACES } from '../../../common/lib/spaces';
+import { DEFAULT_SPACE_ID } from '../../../../../plugins/spaces/common/constants';
+
+export default function ({ getService }) {
+  const supertest = getService('supertest');
+  const esArchiver = getService('esArchiver');
+
+  describe('get', () => {
+
+    const expectResults = (spaceId) => () => (resp) => {
+
+      // The default space does not assign a space id.
+      const expectedSpaceId = spaceId === 'default' ? undefined : spaceId;
+
+      const expectedBody = {
+        id: `${getIdPrefix(spaceId)}dd7caf20-9efd-11e7-acb3-3dab96693fab`,
+        type: 'visualization',
+        updated_at: '2017-09-21T18:51:23.794Z',
+        version: resp.body.version,
+
+        attributes: {
+          title: 'Count of requests',
+          description: '',
+          version: 1,
+          // cheat for some of the more complex attributes
+          visState: resp.body.attributes.visState,
+          uiStateJSON: resp.body.attributes.uiStateJSON,
+          kibanaSavedObjectMeta: resp.body.attributes.kibanaSavedObjectMeta
+        }
+      };
+
+      if (expectedSpaceId) {
+        expectedBody.spaceId = expectedSpaceId;
+      }
+
+      expect(resp.body).to.eql(expectedBody);
+    };
+
+    const expectNotFound = (type, id) => (resp) => {
+      expect(resp.body).to.eql({
+        error: 'Not Found',
+        message: `Saved object [${type}/${id}] not found`,
+        statusCode: 404,
+      });
+    };
+
+    const getTest = (description, { spaceId, tests, otherSpaceId = spaceId }) => {
+      describe(description, () => {
+        before(async () => esArchiver.load(`saved_objects/spaces`));
+        after(async () => esArchiver.unload(`saved_objects/spaces`));
+
+        it(`should return ${tests.exists.statusCode}`, async () => {
+          const objectId = `${getIdPrefix(otherSpaceId)}dd7caf20-9efd-11e7-acb3-3dab96693fab`;
+
+          let expectedObjectId = objectId;
+          const testingMismatchedSpaces = spaceId !== otherSpaceId;
+
+          if (testingMismatchedSpaces && spaceId !== DEFAULT_SPACE_ID) {
+            expectedObjectId = `${spaceId}:${expectedObjectId}`;
+          }
+
+          return supertest
+            .get(`${getUrlPrefix(spaceId)}/api/saved_objects/visualization/${objectId}`)
+            .expect(tests.exists.statusCode)
+            .then(tests.exists.response('visualization', expectedObjectId));
+        });
+      });
+    };
+
+    getTest(`can access objects belonging to the current space (space_1)`, {
+      ...SPACES.SPACE_1,
+      tests: {
+        exists: {
+          statusCode: 200,
+          response: expectResults(SPACES.SPACE_1.spaceId),
+        },
+      }
+    });
+
+    getTest(`cannot access objects belonging to a different space (space_1)`, {
+      ...SPACES.SPACE_1,
+      otherSpaceId: SPACES.SPACE_2.spaceId,
+      tests: {
+        exists: {
+          statusCode: 404,
+          response: expectNotFound
+        },
+      }
+    });
+
+    getTest(`can access objects belonging to the current space (default)`, {
+      ...SPACES.DEFAULT,
+      tests: {
+        exists: {
+          statusCode: 200,
+          response: expectResults(SPACES.DEFAULT.spaceId),
+        },
+      }
+    });
+
+    getTest(`cannot access objects belonging to a different space (default)`, {
+      ...SPACES.DEFAULT,
+      otherSpaceId: SPACES.SPACE_1.spaceId,
+      tests: {
+        exists: {
+          statusCode: 404,
+          response: expectNotFound
+        },
+      }
+    });
+  });
+}
