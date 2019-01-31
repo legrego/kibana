587e918ff4cd93b8a13d17c5384a5c6dbae75201
kobelb <brandon.kobel@elastic.co>
2019-01-30 16:23:32 -0800

    Consolidating Role exports

--- x-pack/plugins/security/common/model/index.ts
+++ x-pack/plugins/security/common/model/index.ts
@@ -4,8 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export { Role } from './role';
+export { Role, RoleIndexPrivilege, RoleKibanaPrivilege } from './role';
 export { FeaturesPrivileges } from './features_privileges';
-export { RoleKibanaPrivilege, RoleIndexPrivilege } from './role';
 export { RawKibanaPrivileges, RawKibanaFeaturePrivileges } from './raw_kibana_privileges';
 export { KibanaPrivileges } from './kibana_privileges';
