ac810b678950834b55cd896827fee022759a3774
kobelb <brandon.kobel@elastic.co>
2019-01-30 16:06:20 -0800

    We only need to export KibanaPrivileges

--- x-pack/plugins/security/common/model/index.ts
+++ x-pack/plugins/security/common/model/index.ts
@@ -7,8 +7,6 @@
 export { Role } from './role';
 export { KibanaFeaturePrivileges, FeaturesPrivileges } from './privileges/feature_privileges';
 export { RoleKibanaPrivilege } from './role';
-export { RawKibanaPrivileges } from './kibana_privilege';
 export { RoleIndexPrivilege } from './index_privilege';
+export { RawKibanaPrivileges } from './kibana_privilege';
 export { KibanaPrivileges } from './privileges/kibana_privileges';
-export { KibanaGlobalPrivileges as GlobalPrivileges } from './privileges/global_privileges';
-export { KibanaSpacesPrivileges as SpacesPrivileges } from './privileges/spaces_privileges';
